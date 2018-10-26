ba4eeda4720f4cf21c134d55fc098e9f6a7b30d4
Alex Nichol <unixpickle@gmail.com>
2018-10-25 21:03:16 -0400

    fix live code updates

--- plugin/popup.js
+++ plugin/popup.js
@@ -8,11 +8,19 @@ chrome.storage.sync.get(['code'], function (result) {
 
 document.getElementById('update-button').onclick = () => {
     const code = codeElem.value;
+    const escaped = JSON.stringify('window.INJECTION_DRAW_FN=' + code + ';');
+    const injectCode = `
+        (function() {
+            const s = document.createElement('script');
+            s.text = ` + escaped + `;
+            s.onload = () => s.remove();
+            (document.head || document.documentElement).appendChild(s);
+        })()`;
     chrome.storage.sync.set({ code: code });
     chrome.tabs.query({}, (tabs) => {
         tabs.forEach((tab) => {
             chrome.tabs.executeScript(tab.id, {
-                code: 'INJECTION_DRAW_FN=' + code + ';'
+                code: injectCode
             }, function () { });
         });
     });
